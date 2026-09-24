18ac16bc87c4c738140cf4083bed387c9ec4c5cc
nyabi-gh <nyabi@tb.pro>
2026-09-25 01:09:57 +0900

    Release 1.3.0
    Bump the version, add the release notes, and have the release workflow publish docs/releases/<tag>.md when it exists.

--- dist/Mollu.plugin.js
+++ dist/Mollu.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name Mollu
  * @author Nyabi
- * @version 1.2.0
+ * @version 1.3.0
  * @description Auto-translates messages in chosen Discord servers into the language you pick, shown under the original.
  * @source https://github.com/nyabi-gh/Mollu
  */
